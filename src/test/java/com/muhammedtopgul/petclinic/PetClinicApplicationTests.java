64a2146b9c0a5bc495c90e9651e52a718d58698c
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-04-05 10:57:40 +0300

    spring uygulamasi olusturuldu

---
+++ src/test/java/com/muhammedtopgul/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,12 @@
+package com.muhammedtopgul.petclinic;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+
+
+}
