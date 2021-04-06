9e14922d76f5223e33fe52e0fe16812fb2509c0d
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-04-07 00:33:11 +0300

    RESTful sorgu endpointleri @ResponseStatus ile yazildi

--- src/main/java/com/muhammedtopgul/petclinic/rest/PetClinicRestController.java
+++ src/main/java/com/muhammedtopgul/petclinic/rest/PetClinicRestController.java
@@ -10,6 +10,7 @@ import com.muhammedtopgul.petclinic.entity.OwnerEntity;
 import com.muhammedtopgul.petclinic.exception.OwnerNotFoundException;
 import com.muhammedtopgul.petclinic.service.PetClinicService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,27 +23,59 @@ public class PetClinicRestController {
 
     private final PetClinicService service;
 
+//    @GetMapping
+//    public ResponseEntity<List<OwnerEntity>> getOwners() {
+//        List<OwnerEntity> owners = service.findOwners();
+//        return ResponseEntity.ok(owners);
+//    }
+
     @GetMapping
-    public ResponseEntity<List<OwnerEntity>> getOwners() {
-        List<OwnerEntity> owners = service.findOwners();
-        return ResponseEntity.ok(owners);
+    @ResponseStatus(HttpStatus.OK)
+    public List<OwnerEntity> getOwners() throws IllegalAccessException {
+        try {
+            return service.findOwners();
+        } catch (Exception exception) {
+            throw new IllegalAccessException();
+        }
     }
+
+//    @GetMapping("/filter")
+//    public ResponseEntity<List<OwnerEntity>> getOwners(@RequestParam("lastName") String lastName) {
+//        List<OwnerEntity> owners = service.findOwners(lastName);
+//        return ResponseEntity.ok(owners);
+//    }
 
     @GetMapping("/filter")
-    public ResponseEntity<List<OwnerEntity>> getOwners(@RequestParam("lastName") String lastName) {
-        List<OwnerEntity> owners = service.findOwners(lastName);
-        return ResponseEntity.ok(owners);
+    @ResponseStatus(HttpStatus.OK)
+    public List<OwnerEntity> getOwners(@RequestParam("lastName") String lastName) throws IllegalAccessException {
+        try {
+            return service.findOwners(lastName);
+        } catch (Exception exception) {
+            throw new IllegalAccessException();
+        }
     }
 
+//    @GetMapping("/{id}")
+//    public ResponseEntity<OwnerEntity> getOwners(@PathVariable("id") Long id) {
+//        try {
+//            OwnerEntity owner = service.findOwner(id);
+//            return ResponseEntity.ok(owner);
+//        } catch (OwnerNotFoundException exception) {
+//            return ResponseEntity.notFound().build();
+//        } catch (Exception exception) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<OwnerEntity> getOwners(@PathVariable("id") Long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public OwnerEntity getOwners(@PathVariable("id") Long id) throws IllegalAccessException {
         try {
-            OwnerEntity owner = service.findOwner(id);
-            return ResponseEntity.ok(owner);
+            return service.findOwner(id);
         } catch (OwnerNotFoundException exception) {
-            return ResponseEntity.notFound().build();
+            throw new OwnerNotFoundException(id + " cannot be found");
         } catch (Exception exception) {
-            return ResponseEntity.badRequest().build();
+            throw new IllegalAccessException();
         }
     }
 }
